38e2fe9da1463b3f8b175f785d8983d28a519e84
YAGOO (not real) <93402922+rosnuker@users.noreply.github.com>
2023-11-18 14:31:41 +0800

    Update RootLayout.jsx

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Button, Typography } from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function RootLayout() {
@@ -6,15 +6,15 @@ export default function RootLayout() {
     <>
       <Grid>
         <nav>
-          <h1>SUGO</h1>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='about'>About Us</NavLink>
-          <NavLink to='contact'>Contact Us</NavLink>
-          <NavLink to='login'>Login</NavLink>
-          <NavLink to='register'>Register</NavLink>
+          <Typography sx={{display: "inline"}}>SUGO</Typography>
+          <NavLink to='/'><Button sx={{marginLeft: 110}}>Home</Button></NavLink>
+          <NavLink to='about'><Button>About Us</Button></NavLink>
+          <NavLink to='contact'><Button>Contact Us</Button></NavLink>
+          <NavLink to='login'><Button>Login</Button></NavLink>
+          <NavLink to='register'><Button>Register</Button></NavLink>
         </nav>
       </Grid>
-      {console.log('Hi')}
+
       <Outlet />
     </>
   )
