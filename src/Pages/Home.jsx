ce2e516c823f090e04f30deaa924c651c920683a
AldrichArisgar <139600282+AldrichArisgar@users.noreply.github.com>
2023-11-18 13:40:43 +0800

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Grid>
-          <h1>Home</h1>
+          <h1>Jeno Gwapo KAAYO SA TANAN</h1>
       </Grid>
       {console.log('Home')}        
     </>
