2380650befe2a8a6b1ac81bad17ea23a313d90f2
YAGOO (not real) <93402922+rosnuker@users.noreply.github.com>
2023-11-22 02:15:33 +0800

    Update RootLayout.jsx

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -4,11 +4,11 @@ import { NavLink, Outlet } from "react-router-dom";
 export default function RootLayout() {
   return (
     <>
-      <Grid container justify='center' alignItems='center' direction='column'>
+      <Grid container justify='center' alignItems='center' direction='column' sx={{marginTop: 1, height: 50}}>
         <Grid item>
           <nav>
             <Typography sx={{display: "inline"}}>SUGO</Typography>
-            <NavLink to='/'><Button sx={{marginLeft: 120}}>Home</Button></NavLink>
+            <NavLink to='/'><Button sx={{marginLeft: 160}}>Home</Button></NavLink>
             <NavLink to='about'><Button>About Us</Button></NavLink>
             <NavLink to='contact'><Button>Contact Us</Button></NavLink>
             <NavLink to='login'><Button variant='contained' sx={{marginLeft: 3}}>Login</Button></NavLink>
