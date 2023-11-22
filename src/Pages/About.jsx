3f69a52b943aa247a21f55b89beaa1ee0d6ed411
CryoKr <jenorochcaballes@gmail.com>
2023-11-22 21:44:09 +0800

    update rako balik taod taod
    mag butang rako sa ato profiles sa ubos later hehe kani lang sa

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,18 +1,19 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 export default function About() {
   return (
-    <>
     <div className='gradientbg_1'>
       <Grid container justify='center' alignItems='center' direction='row'>
+        {/* Image on the left */}
         <Grid item xs={12} sm={6}>
           <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <img src="SUGOWEW" alt="SUGO Image" style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: '5px' }} />
+            <img src="computer_mob_programming.png" alt="SUGO Image" style={{ maxWidth: '450px', maxHeight: '450px', borderRadius: '5px', marginTop: 150, marginLeft:-250 }} />
           </Box>
         </Grid>
         
+        {/* Text on the right */}
         <Grid item xs={12} sm={6}>
-          <Box sx={{ bgcolor: 'white', height: 515, width: 1140, marginTop: 25, marginRight: 70, borderRadius: 5, opacity: '80%', padding: '20px' }}>
+          <Box sx={{ bgcolor: 'white', height: 515, width: 1140, marginTop: 25, marginLeft: -30, borderRadius: 5, opacity: '80%', padding: '20px' }}>
             <Typography sx={{ fontSize: 96, fontWeight: 'bold', marginLeft: 4 }}>
               What is SUGO?
             </Typography>
@@ -20,12 +21,9 @@ export default function About() {
             <Typography sx={{ fontSize: 36, marginLeft: 4 }}>
               SUGO connects you with your fellow students who are willing to lend a helping hand. Whether you need someone to bring you your favorite coffee, return a borrowed book to the library, or simply run an errand. SUGO is here to make your campus life easier and more convenient.
             </Typography>
-            </Box>
-          </Grid>
+          </Box>
         </Grid>
-      </div>
-
-      {console.log('About')}        
-    </>
+      </Grid>
+    </div>
   )
 }
