6f381086f6773b2d88dc512c49bd4c81878021fc
YAGOO (not real) <93402922+rosnuker@users.noreply.github.com>
2023-12-01 04:18:12 +0800

    RootLayout Adjustments

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -8,10 +8,10 @@ export default function RootLayout() {
         <Grid item>
             <AppBar>
               <Toolbar sx={{ bgcolor: 'white' }}>
-                <img src='/images/SugoLogo.png' alt='Sugo Logo' height='55px' style={{ marginRight: '10px', marginLeft: '45px' }}></img>
+                <img src='/images/SugoLogo.png' alt='Sugo Logo' height='55px' style={{ marginRight: '10px', marginLeft: '100px' }}></img>
                 <img src='/images/SugoBanner.png' alt='Sugo Banner' height='55px'></img>
                 <nav>
-                  <NavLink to='/'><Button sx={{marginLeft: { xs: 0, sm: 1, md: 7, lg: 44, xl: 87 }}}>Home</Button></NavLink>
+                  <NavLink to='/'><Button sx={{marginLeft: { xs: 0, sm: 1, md: 4, lg: 40, xl: 85 }}}>Home</Button></NavLink>
                   <NavLink to='about'><Button>About Us</Button></NavLink>
                   <NavLink to='contact'><Button>Contact Us</Button></NavLink>
                   <NavLink to='login'><Button variant='contained' sx={{marginLeft: 3}}>Login</Button></NavLink>
