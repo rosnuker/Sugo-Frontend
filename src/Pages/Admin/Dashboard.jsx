e26b5dd7d58260441cdbeb7e2ca29fe0d5882187
YAGOO (not real) <93402922+rosnuker@users.noreply.github.com>
2023-12-01 12:33:24 +0800

    Initial Implementation of Admin Side

--- src/Pages/Admin/Dashboard.jsx
+++ src/Pages/Admin/Dashboard.jsx
@@ -1,8 +1,46 @@
+import { Box, Grid, Paper, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 export default function DashboardAdmin() {
-    return (
-        <div className="gradientbg_2">
-
-        </div>
-    )
+  return (
+    <div className="gradientbg_2">
+      <Grid container justify='center' alignItems='center' direction='column'>
+        <Box sx={{ bgcolor: '#e6e6e6', height: 785, width: 1600, marginLeft: 30, marginTop: 15, borderRadius: 7 }}>
+            <TableContainer component={Paper} sx={{marginTop: 10, marginLeft: 70, height: 365, width: 500}}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Dessert (100g serving)</TableCell>
+                  <TableCell align='right'>Calories</TableCell>
+                  <TableCell align='right'>Fat&nbsp;(g)</TableCell>
+                  <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
+                  <TableCell align='right'>Protein&nbsp;(g)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.name} sx={{ '&: last-child td, &: last-child th': { border: 0} }}>
+                    <TableCell component='th' scope='row'>{row.name}</TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                    <TableCell align="right">{row.protein}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </TableContainer>
+        </Box>
+      </Grid>
+    </div>
+  )
 }
