084836772ace99a335d9b6a4c990bdf371a9475f
AldrichArisgar <139600282+AldrichArisgar@users.noreply.github.com>
2023-12-20 01:33:12 +0800

    Login Courier Dashboard &  Separate Drawer

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -9,53 +9,66 @@ export default function Login() {
   const [loginData, setLoginData] = useState({
     email: '',
     password: ''
-  })
+  });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setLoginData((prevData) => ({ ...prevData, [name]: value }));
-  }
+  };
 
   const onSubmit = async (e) => {
-    await axios.post('http://localhost:8080/loginUser', {
-      email: loginData.email,
-      password: loginData.password
-    }, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-      if (!(response.status === 200)) {
-        throw new Error('There is a problem with the request');
-      }
-      if(response.data === 'Login Success!') {
-        navigate('/customer');
-      }
-      // console.log(response.data)
-    }).catch(error => {
-      console.log('There was a problem with the fetch operation:', error)
-    })
-  }
+    try {
+      const response = await axios.post('http://localhost:8080/loginUser', {
+        email: loginData.email,
+        password: loginData.password
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
 
-  return(
+      if (response.status === 200 && response.data === 'Login Success!') {
+        navigate('/customer');
+      } else {
+        const courierResponse = await axios.post('http://localhost:8080/loginCourier', {
+          email: loginData.email,
+          password: loginData.password
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (courierResponse.status === 200 && courierResponse.data === 'Login Success!') {
+          navigate('/courier');
+        } else {
+          console.log('Login failed for both customer and courier');
+        }
+      }
+    } catch (error) {
+      console.log('There was a problem with the login request:', error);
+    }
+  };
+
+  return (
     <>
       <div className='gradientbg_2'>
         <Grid container justify='center' alignItems='center' direction='column'>
           <Grid item>
             <Paper elevation={4} sx={{ bgcolor: 'white', height: 700, width: 600, marginTop: 15, borderRadius: 5 }}>
               <Box>
-                <img src='./images/Logo.png' alt='Sugo Logo' style={{marginLeft: '140px', marginTop: '60px'}} />
+                <img src='./images/Logo.png' alt='Sugo Logo' style={{ marginLeft: '140px', marginTop: '60px' }} />
               </Box>
 
               <TextField name='email' label='Email' onChange={handleChange} size='normal' type='email' variant='standard' sx={{ width: 460, marginLeft: 8, marginTop: 4 }} />
-              <TextField name= 'password' label='Password' onChange={handleChange} size='normal' type='password' variant='standard' sx={{ width: 460, marginLeft: 8, marginTop: 2 }} />
+              <TextField name='password' label='Password' onChange={handleChange} size='normal' type='password' variant='standard' sx={{ width: 460, marginLeft: 8, marginTop: 2 }} />
               <Button variant='contained' onClick={onSubmit} size='large' sx={{ bgcolor: '#333DAD', marginTop: 5, marginLeft: 8, marginBottom: 3, width: 460 }}>Login</Button>
-              <Typography sx={{ marginLeft: 20, display: 'inline'}}>Don't have an account yet? </Typography>
+              <Typography sx={{ marginLeft: 20, display: 'inline' }}>Don't have an account yet? </Typography>
               <NavLink to='/register'><Typography sx={{ display: 'inline' }}>Register</Typography></NavLink>
             </Paper>
           </Grid>
         </Grid>
       </div>
     </>
-  )
+  );
 }
